ccd8069774a37d70d5fa82c9b86df0b04ef67de2
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-03-09 23:15:05 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from '../components/header/header.component';
+import { HeroComponent } from '../components/hero/hero.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'QR-Ordering-System';
+}
