3dd0beb91812099a127fbd85d286b029f014acbd
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-04-06 16:38:12 +0530

    updated

--- src/components/menu-page/menu-page.component.ts
+++ src/components/menu-page/menu-page.component.ts
@@ -1,32 +1,36 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {FooterComponent} from '../footer/footer.component';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {FoodItemService} from '../../shared/services/foodItems/food-item.service';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {AsyncPipe} from '@angular/common';
 import {FoodItemComponent} from '../../shared/food-item/food-item.component';
-import {foodInterface} from '../../model/food.interface';
 import {MenuFooterComponent} from '../menu-footer/menu-footer.component';
+import {foodInterface} from '../../model/food.interface';
 
 @Component({
   selector: 'app-menu-page',
   imports: [
-    FooterComponent,
-    AsyncPipe,
     FoodItemComponent,
     MenuFooterComponent,
   ],
   templateUrl: './menu-page.component.html',
   styleUrl: './menu-page.component.css'
 })
-export class MenuPageComponent implements OnInit{
+export class MenuPageComponent implements OnInit {
 
-  private http = inject(HttpClient);
   private foodApi = inject(FoodItemService);
-  foodItem$ = this.foodApi.getFoodItems() as Observable<foodInterface[]>;
+  foodItem = signal<foodInterface[]>([]);
+  isLoading = signal(true);
+  hasError = signal(false);
 
   ngOnInit() {
-    console.log(this.foodApi.getFoodItems().subscribe(data => console.log(data)));
+    this.foodApi.getFoodItems().subscribe({
+      next: (data) => {
+        this.foodItem.set(data);
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        console.error('Error fetching food items:', err);
+        this.hasError.set(true);
+        this.isLoading.set(false);
+      }
+    });
   }
-
 }
