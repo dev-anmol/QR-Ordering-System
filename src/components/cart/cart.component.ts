8bc0438ffc58a90831eab389a8f69e8d5afd9d24
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-04-12 11:56:17 +0530

    fixed

--- src/components/cart/cart.component.ts
+++ src/components/cart/cart.component.ts
@@ -1,9 +1,11 @@
 import {Component, inject, signal, WritableSignal} from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState } from '../../state/app.state';
-import { selectCartState } from '../../state/cart/cart.selector';
-import { AsyncPipe } from '@angular/common';
-import { MenuFooterComponent } from '../menu-footer/menu-footer.component';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../state/app.state';
+import {selectCartState} from '../../state/cart/cart.selector';
+import {AsyncPipe} from '@angular/common';
+import {MenuFooterComponent} from '../menu-footer/menu-footer.component';
+import {foodInterface} from '../../model/food.interface';
+import {removeItem} from '../../state/cart/cart.actions';
 
 @Component({
   selector: 'app-cart',
@@ -15,11 +17,14 @@ import { MenuFooterComponent } from '../menu-footer/menu-footer.component';
 export class CartComponent {
   private store = inject<Store<AppState>>(Store);
   public foodItem$ = this.store.select(selectCartState);
-  public foodItemLength : WritableSignal<any> = signal(0)
-  getListSize() {
-    this.foodItemLength.set(this.foodItem$);
-    console.log(this.foodItemLength.length);
-    return this.foodItemLength.length;
+  public foodItemLength: WritableSignal<any> = signal(0)
+
+  removeItem(product: foodInterface) {
+    if (product.id !== undefined) {
+      this.store.dispatch(removeItem({productId: product.id}));
+    } else {
+      console.error('Product ID is undefined');
+    }
   }
 
 
