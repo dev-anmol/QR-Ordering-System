6fcd90697f92c24e12c8bd25e8ea719bdaa74840
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-06-09 21:51:55 +0530

    updated

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private router = inject(Router);
 
+  email : WritableSignal<string> = signal('');
+  password : WritableSignal<string> = signal('');
+
+  handleEmail(event: Event) {
+    const inputVal = (event.target as HTMLInputElement).value;
+    this.email.set(inputVal); 
+  }
+
+  handlePassword(event: Event) {
+    const inputVal = (event.target as HTMLInputElement).value;
+    this.password.set(inputVal);
+  }
+
+  handleSubmit(event : Event) {
+      event.preventDefault();
+      const signupForm = {
+        email: this.email(),
+        password: this.password()
+      }
+      console.log(signupForm);
+      this.email.update(prev => prev = '')
+      this.password.update(prev => prev = '')
+  }
 }
