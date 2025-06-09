8d35498ad9d21a032da10f30e0d818f821b0ae7c
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-06-09 22:42:00 +0530

    updated

--- src/components/signup/signup.component.ts
+++ src/components/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -41,6 +41,7 @@ export class SignupComponent {
       this.email.update(prev => prev = '')
       this.name.update(prev => prev = '')
       this.password.update(prev => prev = '')
+      this.router.navigateByUrl('/home')
   }
 
 
