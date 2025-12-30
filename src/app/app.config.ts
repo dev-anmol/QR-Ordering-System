ef9a0471fb28f2afd1e75db52728b4de2d5e7026
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-12-30 23:51:16 +0530

    Added server side rendering

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,25 +1,23 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZonelessChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideState, provideStore } from '@ngrx/store';
-import { provideToastr } from 'ngx-toastr';
-import { CartReducer } from '../state/cart/cart.reducer';
-import { routes } from './app.routes';
-
+import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideState, provideStore} from '@ngrx/store';
+import {provideToastr} from 'ngx-toastr';
+import {CartReducer} from '../state/cart/cart.reducer';
+import {routes} from './app.routes';
+import {provideClientHydration, withEventReplay, withIncrementalHydration} from '@angular/platform-browser';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideStore(),
-    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
+    provideClientHydration(withIncrementalHydration()),
     provideState({ name: 'products', reducer: CartReducer }),
-    provideAnimations(),
-    provideAnimationsAsync(),
     provideToastr({
       positionClass: 'toast-top-right',
       timeOut: 3000,
