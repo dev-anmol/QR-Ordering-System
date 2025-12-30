a4fe0aec344d16e716e3a899ca16dd52710c2910
Anmol Thakur <anmolll.thakurrr@gmail.com>
2025-12-30 22:48:39 +0530

    migrated to angular 21

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,11 +1,11 @@
 import {Component, inject, signal, WritableSignal} from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
-import {CommonModule} from '@angular/common';
+
 import {UicartService} from '../../shared/services/uicart/uicart.service';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLink],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
